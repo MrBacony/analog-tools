b2f8b30af1212d8e83bd78f83997925b7161aadb
Gregor Speck <gregor.speck@bridging-it.de>
2025-04-27 09:35:35 +0200

    feat: added example app for testing the features

---
+++ apps/analog-example/src/app/pages/(home).page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { AnalogWelcomeComponent } from './analog-welcome.component';
+
+@Component({
+  selector: 'analog-example-home',
+
+  imports: [AnalogWelcomeComponent],
+  template: ` <analog-example-analog-welcome /> `,
+})
+export default class HomeComponent {}
