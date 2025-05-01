75f06a65f5428644a585c8b703809e85bf8233a1
Gregor Speck <gregor.speck@bridging-it.de>
2025-05-01 09:01:18 +0200

    refactor(auth): comment out unused exports in index file

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,7 +1,14 @@
-export * from './services/session.service';
-export * from './services/oauth-authentication.service';
-export * from './functions/handleAuthRoute';
-export * from './functions/useAnalogAuthMiddleware';
+//export * from './services/session.service';
+//export * from './services/oauth-authentication.service';
+//export * from './functions/handleAuthRoute';
+//export * from './functions/useAnalogAuthMiddleware';
 export * from './functions/useAnalogAuth';
 
-export * from './types/auth.types';
+export {
+  AnalogAuthConfig,
+  CookieSessionConfig,
+  MemorySessionConfig,
+  RedisSessionConfig,
+  SessionStorageConfig,
+  UserHandler,
+} from './types/auth.types';
