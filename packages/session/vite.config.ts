2cdd6e0f58bce30c73b42474ef6c2a7b5ae5ebc4
Gregor Speck <gregor.speck@bridging-it.de>
2025-05-14 21:38:52 +0200

    feat(session): update build config

--- packages/session/vite.config.ts
+++ packages/session/vite.config.ts
@@ -1,15 +1,56 @@
 import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import dts from 'vite-plugin-dts';
+import * as path from 'path';
 
 export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/packages/session',
-  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md']),
+    dts({
+      entryRoot: 'src',
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+    }),],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
+  build: {
+    outDir: '../../node_modules/@analog-tools/session',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: 'session',
+      fileName: (format) => {
+        let prefix = 'js';
+        if (format === 'cjs') {
+          prefix = 'cjs';
+        }
+
+        return `index.${prefix}`;
+      },
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es' as const, 'cjs' as const],
+    },
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: [
+        'node:buffer',
+        'h3',
+        'defu',
+        'uncrypto',
+        'unstorage',
+        'ioredis'
+      ],
+    },
+  },
   test: {
     watch: false,
     globals: true,
