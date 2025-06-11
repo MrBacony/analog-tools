e85bc66eac7028921920552563ef01615fef3909
Gregor Speck <gregor.speck@bridging-it.de>
2025-06-11 21:13:40 +0200

    feat(auth): extract authentication configuration to auth.config.ts

--- apps/analog-example/src/server/middleware/auth.ts
+++ apps/analog-example/src/server/middleware/auth.ts
@@ -1,21 +1,6 @@
-import { AnalogAuthConfig, useAnalogAuth } from '@analog-tools/auth';
+import { useAnalogAuth } from '@analog-tools/auth';
 import { defineEventHandler, H3Event } from 'h3';
-
-const authConfig: AnalogAuthConfig = {
-  issuer: process.env['AUTH_ISSUER'] || '',
-  clientId: process.env['AUTH_CLIENT_ID'] || '',
-  clientSecret: process.env['AUTH_CLIENT_SECRET'] || '',
-  audience: process.env['AUTH_AUDIENCE'] || '',
-  scope: process.env['AUTH_SCOPE'] || '',
-  callbackUri: process.env['AUTH_CALLBACK_URL'] || '',
-  sessionStorage: {
-    type: 'redis',
-    config: {
-      url: process.env['REDIS_HOST'] || 'localhost',
-      port: process.env['REDIS_PORT'] || 6379,
-    },
-  },
-};
+import { authConfig } from '../../auth.config';
 
 /**
  * Authentication middleware for protected API routes
