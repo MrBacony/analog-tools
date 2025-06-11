8ebcc0c35ca0979c25d98877d4f96243c43b486b
Gregor Speck <gregor.speck@bridging-it.de>
2025-06-11 21:24:05 +0200

    refactor(service): improve service registration logic and use injectable names

--- packages/inject/src/lib/service-registry.ts
+++ packages/inject/src/lib/service-registry.ts
@@ -5,6 +5,7 @@ import { InjectionServiceClass } from './inject.types';
  */
 
 export let _serviceRegistry: ServiceRegistry | null = null;
+
 export function getServiceRegistry(): ServiceRegistry {
   if (!_serviceRegistry) {
     _serviceRegistry = new ServiceRegistry();
@@ -17,7 +18,7 @@ export function getServiceRegistry(): ServiceRegistry {
  * Implements the singleton pattern for central service management.
  */
 export class ServiceRegistry {
-  private serviceMap: Map<InjectionServiceClass<unknown>, unknown> = new Map();
+  private serviceMap: Map<string, unknown> = new Map();
   /**
    * Register a service with a token
    * @param token - The injection token for the service
@@ -28,26 +29,27 @@ export class ServiceRegistry {
     ...properties: ConstructorParameters<InjectionServiceClass<T>>
   ): void {
     if (this.isServiceInjectable(token)) {
-      if (
-        !this.serviceMap.has(token) ||
-        this.serviceMap.get(token) === undefined
-      ) {
+      if (!this.hasService(token) || this.getService(token) === undefined) {
         if (properties === undefined || properties.length === 0) {
-          this.serviceMap.set(token, new token());
+          this.serviceMap.set(this.getInjcectableName(token), new token());
           return;
         }
-
-        this.serviceMap.set(token, new token(...properties));
+        this.serviceMap.set(
+          this.getInjcectableName(token),
+          new token(...properties)
+        );
       }
     }
   }
 
   public registerAsUndefined<T>(token: InjectionServiceClass<T>): void {
     if (this.isServiceInjectable(token)) {
-      this.serviceMap.set(token, undefined);
+      this.serviceMap.set(this.getInjcectableName(token), undefined);
     } else {
       throw new Error(
-        `Service with token ${token.name} is not injectable. Ensure it has the INJECTABLE static property set to true.`
+        `Service with token ${this.getInjcectableName(
+          token
+        )} is not injectable. Ensure it has the INJECTABLE static property set to true.`
       );
     }
   }
@@ -57,10 +59,12 @@ export class ServiceRegistry {
     customObject: Partial<T>
   ): void {
     if (this.isServiceInjectable(token)) {
-      this.serviceMap.set(token, customObject);
+      this.serviceMap.set(this.getInjcectableName(token), customObject);
     } else {
       throw new Error(
-        `Service with token ${token.name} is not injectable. Ensure it has the INJECTABLE static property set to true.`
+        `Service with token ${this.getInjcectableName(
+          token
+        )} is not injectable. Ensure it has the INJECTABLE static property set to true.`
       );
     }
   }
@@ -75,7 +79,9 @@ export class ServiceRegistry {
       if (!this.hasService(token)) {
         this.register(token);
       }
-      return this.serviceMap.get(token) as T | undefined;
+      return this.serviceMap.get(this.getInjcectableName(token)) as
+        | T
+        | undefined;
     }
     return undefined;
   }
@@ -86,7 +92,7 @@ export class ServiceRegistry {
    * @returns True if the service is registered
    */
   public hasService<T>(token: InjectionServiceClass<T>): boolean {
-    return this.serviceMap.has(token);
+    return this.serviceMap.has(this.getInjcectableName(token));
   }
 
   /**
@@ -95,10 +101,25 @@ export class ServiceRegistry {
    * @returns True if the service has the INJECTABLE static property set to true
    */
   public isServiceInjectable<T>(token: InjectionServiceClass<T>): boolean {
-    const injectable = (token as unknown as { INJECTABLE?: boolean })
+    const injectable = (token as unknown as { INJECTABLE?: boolean | string })
       .INJECTABLE;
-    return injectable !== undefined && injectable;
+    return injectable !== undefined && injectable !== false;
   }
+
+  public getInjcectableName<T>(token: InjectionServiceClass<T>): string {
+    if (this.isServiceInjectable(token)) {
+      const injectable = (token as unknown as { INJECTABLE?: string | boolean })
+        .INJECTABLE;
+      if (typeof injectable !== 'string') {
+        return token.name;
+      }
+      return injectable;
+    }
+    throw new Error(
+      `Service with token ${token.name} is not injectable. Ensure it has the INJECTABLE static property set to true.`
+    );
+  }
+
   /**
    * Clean up all services when the game is being destroyed
    */
