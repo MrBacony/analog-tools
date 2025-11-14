417644c99c6f827e302864e5575b10726159110e
Gregor Speck <gregor.speck@bridging-it.de>
2025-11-14 10:27:04 +0100

    style(storage): add vite-ignore comment for async driver import

--- packages/session/src/storage.ts
+++ packages/session/src/storage.ts
@@ -7,7 +7,8 @@ import type { DriverOptions, SessionData } from './types';
 
 
 export async function createUnstorageStore<T extends SessionData = SessionData>(options: DriverOptions): Promise<Storage<T>> {
-  const driver = await import(builtinDrivers[options.type]);
+
+  const driver = await import( /* @vite-ignore */builtinDrivers[options.type]);
   // async import of the driver based on the name
   if(options.type === 'memory') {
     return createStorage({
